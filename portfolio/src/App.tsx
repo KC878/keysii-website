c22fc2001c673ddbbf24a82a0fb139a958697acb
Kent Chrisitan <kentcagadas19@gmail.com>
2025-09-06 23:15:17 +0800

    added UI layout and initial design for contentOne --leftside of the content

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,8 +1,13 @@
 import "./styles/index.css";
+import "./styles/contentOneLeft.css";
+
 import Header from "./components/Header";
 import { useState } from "react";
 import type { HeaderItems } from "./types/types"; // explicityly define type
 import { logos } from "./constants/logos";
+
+import { MdCallMade } from "react-icons/md";
+import { FaArrowDownLong } from "react-icons/fa6";
 
 const App = () => {
   const [headerItems] = useState<HeaderItems[]>([
@@ -13,6 +18,9 @@ const App = () => {
     { id: "pages", title: "PAGES" },
     { id: "contact", title: "CONTACT" },
   ]);
+
+  const [hireMeHover, setHireMeHover] = useState<boolean>(false);
+  const [downArrowHover, setDownArrowHover] = useState<boolean>(false);
 
   console.log(typeof headerItems);
 
@@ -81,8 +89,69 @@ const App = () => {
             </div>
           </div>
         </div>
-        <div className="content">Content</div>
-        <div className="sidebar">Sidebar</div>
+
+        <div className="content">
+          <div className="content-one">
+            {/* Left Side */}
+            <div className="leftContentOne-container">
+              <div className="textOne">Welcome To my World</div>
+              <div className="textTwo">Hello, I am</div>
+              <div className="textThree">Kent Christian E. Cagadas</div>
+              <div className="textFour">
+                Full Stack Developer / Data Analyst
+              </div>
+              <div className="textFive">
+                Who says there are no shortcut in life? When I literally
+                invented one.
+              </div>
+              <div className="textSix">
+                <div
+                  className="hireMe-button"
+                  style={{
+                    borderStyle: hireMeHover ? "inset" : "outset",
+                    backgroundColor: hireMeHover ? "lightskyblue" : "#ced2d7",
+                    cursor: "pointer",
+                  }}
+                  onMouseEnter={() => setHireMeHover(true)}
+                  onMouseLeave={() => setHireMeHover(false)}
+                >
+                  <div>Hire Me</div>
+                  <MdCallMade />
+                </div>
+              </div>
+              <div className="TextSeven">
+                <div
+                  className="downArrow-button"
+                  style={{
+                    backgroundColor: downArrowHover ? "lightblue" : "initial",
+                    cursor: "pointer",
+                    color: downArrowHover ? "black" : "azure",
+                  }}
+                  onMouseEnter={() => setDownArrowHover(true)}
+                  onMouseLeave={() => setDownArrowHover(false)}
+                >
+                  <FaArrowDownLong />
+                </div>
+              </div>
+            </div>
+            <div
+              style={{
+                backgroundColor: "green",
+              }}
+            >
+              {" "}
+            </div>
+            <div
+              style={{
+                backgroundColor: "black",
+              }}
+            >
+              {" "}
+            </div>
+          </div>
+          <div className="content-list">List Items</div>
+          <div className="content-two">Content 2 </div>
+        </div>
         <div className="footer">Footer</div>
       </div>
     </>
