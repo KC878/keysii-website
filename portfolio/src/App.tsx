05b81bc8fff87efea9c297796762512126c2d9f2
Kent Chrisitan <kentcagadas19@gmail.com>
2025-09-06 15:30:36 +0800

    initial-setup

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,16 @@
+import "./styles/index.css";
+
+function App() {
+  return (
+    <>
+      <div className="main-container">
+        <div className="header">Header</div>
+        <div className="content">Content</div>
+        <div className="sidebar">Sidebar</div>
+        <div className="footer">Footer</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ setup-guide.md
@@ -0,0 +1,5 @@
+npm create vite@latest . -- --template react-ts
+
+cd to project directory
+
+npm install
